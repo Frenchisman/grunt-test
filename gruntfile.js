35944d5f27496d333356f3ff133edded7d01b559
Gaël Le Bon <gael.m.lebon@gmail.com>
2016-10-17 14:33:23 +0200

    Test ftpPut

--- gruntfile.js
+++ gruntfile.js
@@ -1,21 +1,25 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-    copy: {
-    main: {
-      files: [
-        {src: ['node_modules/grunt/**'], dest: 'copy/'}, // includes files in path 
-        {expand: true, cwd: 'path/', src: ['**'], dest: 'dest/'}, // makes all src relative to cwd 
-        {expand: true, flatten: true, src: ['path/**'], dest: 'dest/', filter: 'isFile'} // flattens results to a single level 
-      ]
-    }
-  },
+     ftpPut: {
+        options: {
+            host: 'h3p.eu',
+            user: 'Tcdi',
+            pass: 'poitiers'
+        },
+        upload: {
+            files: {
+                'public_html': 'fi.le'
+            }
+        }
+    },
     watch: {
         }
   });
 
   grunt.loadNpmTasks('dp-grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default', ['copy']);
+  grunt.loadNpmTasks('grunt-ftp');
+  grunt.registerTask('default', ['ftpPut']);
 
 };
